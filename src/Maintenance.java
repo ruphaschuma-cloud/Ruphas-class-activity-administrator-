b05408558ec63299f7e5c9aa44148c3dbc813486
ruphaschuma-cloud <ruphas.chuma@strathmore.edu>
2026-03-31 18:39:39 +0300

    Refactor Maintenance interface to class implementation

--- src/Maintenance.java
+++ src/Maintenance.java
@@ -1,5 +1,14 @@
 // Interface: Maintenance
-public interface Maintenance {
-    void scheduleService();
-    void performCheckup();
+public class Maintenance{
+    private string VehicleID;
+    private string details;
+
+    public Maintenance(String vehicleID, String details){
+        this.vehicleID = vehicleID;
+        this.details = details;
+    }
+    public void scheduleService(){
+        System.out.println("Scheduling service for vehicle" + vehicleID +
+                            "|Details:"+ details);
+    }
 }
